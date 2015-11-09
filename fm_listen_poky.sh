3afbd0abc603f1c4ae53d5af90e817277a5acb00
Sarweshkumar C R <sarweshkumar47@gmail.com>
2015-11-09 13:04:37 +0530

    Update fm_listen_poky.sh

--- fm_listen_poky.sh
+++ fm_listen_poky.sh
@@ -4,5 +4,6 @@ if [ $# -eq 0 ]
     echo ""
     echo "Error: Please specify the frequency as an argument in MHz."
     echo ""
+    exit 1
 fi
 rtl_fm -f $1M -M fm -s 170k -A fast -r 32k -l 0 -E deemp -g 30 | aplay -r 30k -t raw -f S16_LE -c 1 -V1 -
