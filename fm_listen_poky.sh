a56101bd3a736356e1699714c339ca5b0fde9c2a
Sarweshkumar C R <sarweshkumar47@gmail.com>
2016-01-20 11:06:29 +0530

    Update fm_listen_poky.sh

--- fm_listen_poky.sh
+++ fm_listen_poky.sh
@@ -6,4 +6,5 @@ if [ $# -eq 0 ]
     echo ""
     exit 1
 fi
-rtl_fm -f $1M -M fm -s 170k -A fast -r 32k -l 0 -E deemp -g 30 | aplay -r 30k -t raw -f S16_LE -c 1 -V1 -
+rtl_fm -f $1e6 -M wbfm -s 200000 -r 48000 - | aplay -r 48k -f S16_LE
+#rtl_fm -f $1M -M fm -s 170k -A fast -r 32k -l 0 -E deemp -g 30 | aplay -r 30k -t raw -f S16_LE -c 1 -V1 -
